c134369c648402cdad1d0de81e4728ffd320051d
Pedro <pedrofbr@hotmail.com>
2024-08-21 14:01:39 -0300

    fix: calling oauth service

--- youtube_downloader/app.py
+++ youtube_downloader/app.py
@@ -19,17 +19,17 @@ REDIRECT_URI = "http://localhost:8080/"
 # Adding credentials to OAuth2
 def get_authenticated_service():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists(TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
     
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CLIENT_SECRETS_FILE, SCOPES, redirect_uri=REDIRECT_URI)
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
             creds = flow.run_local_server(port=8501)
-        with open('token.json', 'w') as token:
+        # Salva o token para reutilização futura
+        with open(TOKEN_FILE, 'w') as token:
             token.write(creds.to_json())
     
     return creds
@@ -59,10 +59,14 @@ def format_selector(ctx):
         # Must be + separated list of protocols
         'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
     }
+    
+st.title('YouTube Video Downloader')
 
 
 #Streamlit interface
 video_url = st.text_input('Video URL')
+download_url = None
+service = get_authenticated_service()
 
 ydl_opts = {}
 
@@ -75,8 +79,23 @@ ydl_opts = {}
 if video_url:
     ydl_opts = {'format': format_selector}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_url])
-        
+        # ydl.download([video_url])
+    
+    
+        if st.button('Gerar Link de Download'):
+            info_dict = ydl.extract_info(video_url, download=False)
+            sanitized_info = ydl.sanitize_info(info_dict)
+            # video_url = sanitized_info['url']
+            with open("data.json", "w") as file:
+                json.dump(sanitized_info, file)
+            video_url = info_dict['url']
+            # selected_format_id = format_list[format_options.index(selected_format)][0]
+            st.success('Link de download gerado! Clique abaixo para baixar o vídeo.')
+            st.markdown(f"[Download Video]({video_url})", unsafe_allow_html=True)
+
+    if download_url:
+        st.markdown(f"[Download Video]({download_url})", unsafe_allow_html=True)
+
 # # Interface do Streamlit
 # url = st.text_input('Insira a URL do vídeo do YouTube:')
 # download_url = None
@@ -89,7 +108,7 @@ if video_url:
 #         # Use `.get()` para evitar o KeyError se 'format_note' não existir
 #         format_options = [f"{f.get('format_note', 'Unknown')} ({f.get('width', 'N/A')}x{f.get('height', 'N/A')})" for f in formats if 'vcodec' in f and 'acodec' in f]
 #         selected_format = st.selectbox('Escolha a qualidade de download:', format_options)
-        
+        q
 #         if st.button('Download'):
 #             selected_format_info = formats[format_options.index(selected_format)]
 #             video_url = selected_format_info['url']
